4474ef66b1d4adb0d1fe209cea85466ad6bb415c
kwfisher <fisher.kolton@gmail.com>
2025-09-17 15:03:17 -0500

    feat(ui): add button to MainActivity to enter VacationList

--- app/src/main/java/com/kwfisher/vacationscheduler/UI/MainActivity.java
+++ app/src/main/java/com/kwfisher/vacationscheduler/UI/MainActivity.java
@@ -1,6 +1,9 @@
 package com.kwfisher.vacationscheduler.UI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +24,16 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button enterButton = findViewById(R.id.enter_button);
+
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, VacationList.class);
+                startActivity(intent);
+            }
         });
     }
 }
