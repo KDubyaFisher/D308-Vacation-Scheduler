e82defa016fc468c10ad2b420f7720431ade8808
kwfisher <fisher.kolton@gmail.com>
2025-09-19 16:37:56 -0500

    feat(database): add database annotation to VacationDatabaseBuilder class

--- app/src/main/java/com/kwfisher/vacationscheduler/database/VacationDatabaseBuilder.java
+++ app/src/main/java/com/kwfisher/vacationscheduler/database/VacationDatabaseBuilder.java
@@ -1,4 +1,10 @@
 package com.kwfisher.vacationscheduler.database;
 
+import androidx.room.Database;
+
+import com.kwfisher.vacationscheduler.entities.Excursion;
+import com.kwfisher.vacationscheduler.entities.Vacation;
+
+@Database(entities = {Vacation.class, Excursion.class}, version = 1, exportSchema = false)
 public class VacationDatabaseBuilder {
 }
