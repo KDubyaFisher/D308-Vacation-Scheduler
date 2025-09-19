16b7276089b4a379f83b40749743f6c3525917cd
kwfisher <fisher.kolton@gmail.com>
2025-09-19 17:21:19 -0500

    feat(database): add DAO and executor fields to Repository.java

--- app/src/main/java/com/kwfisher/vacationscheduler/database/Repository.java
+++ app/src/main/java/com/kwfisher/vacationscheduler/database/Repository.java
@@ -1,4 +1,22 @@
 package com.kwfisher.vacationscheduler.database;
 
+import com.kwfisher.vacationscheduler.dao.ExcursionDAO;
+import com.kwfisher.vacationscheduler.dao.VacationDAO;
+import com.kwfisher.vacationscheduler.entities.Excursion;
+import com.kwfisher.vacationscheduler.entities.Vacation;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Repository {
+
+    private VacationDAO mVacationDAO;
+    private ExcursionDAO mExcursionDAO;
+
+    private List<Vacation> mAllVacations;
+    private List<Excursion> mAllExcursions;
+
+    private static int NUMBER_OF_THREADS = 4;
+    static final ExecutorService databaseExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
 }
