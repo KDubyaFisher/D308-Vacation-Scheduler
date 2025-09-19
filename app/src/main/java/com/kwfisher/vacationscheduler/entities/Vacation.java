b3aaaa52843cd52f53924579dd6df3e4fe3d9fad
kwfisher <fisher.kolton@gmail.com>
2025-09-19 16:31:18 -0500

    feat(entities): add fields to Vacation entity

--- app/src/main/java/com/kwfisher/vacationscheduler/entities/Vacation.java
+++ app/src/main/java/com/kwfisher/vacationscheduler/entities/Vacation.java
@@ -1,7 +1,15 @@
 package com.kwfisher.vacationscheduler.entities;
 
 import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 @Entity(tableName = "vacations")
 public class Vacation {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String vacationTitle;
+    private String vacationLodging;
+    private String vacationStartDate;
+    private String vacationEndDate;
 }
