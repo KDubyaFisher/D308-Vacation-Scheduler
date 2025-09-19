5420169f5040cd382cd9f51ec908323d3a69b905
kwfisher <fisher.kolton@gmail.com>
2025-09-19 17:11:54 -0500

    feat(database): add database instance and builder

--- app/src/main/java/com/kwfisher/vacationscheduler/database/VacationDatabaseBuilder.java
+++ app/src/main/java/com/kwfisher/vacationscheduler/database/VacationDatabaseBuilder.java
@@ -1,6 +1,9 @@
 package com.kwfisher.vacationscheduler.database;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import com.kwfisher.vacationscheduler.dao.ExcursionDAO;
@@ -14,4 +17,17 @@ public abstract class VacationDatabaseBuilder extends RoomDatabase {
     public abstract VacationDAO vacationDAO();
     public abstract ExcursionDAO excursionDAO();
     private static volatile VacationDatabaseBuilder INSTANCE;
+
+    static VacationDatabaseBuilder getDatabase(final Context context){
+        if (INSTANCE == null) {
+            synchronized (VacationDatabaseBuilder.class){
+                if (INSTANCE == null){
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), VacationDatabaseBuilder.class, "MyVacationDatabase.db")
+                            .fallbackToDestructiveMigration(true)
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
