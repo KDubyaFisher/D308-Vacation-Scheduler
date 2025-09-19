8fbba3930787122207c7133c16191eb21dde4268
kwfisher <fisher.kolton@gmail.com>
2025-09-19 17:01:02 -0500

    feat(database): add DAOs and instance to VacationDatabaseBuilder

--- app/src/main/java/com/kwfisher/vacationscheduler/database/VacationDatabaseBuilder.java
+++ app/src/main/java/com/kwfisher/vacationscheduler/database/VacationDatabaseBuilder.java
@@ -3,9 +3,15 @@ package com.kwfisher.vacationscheduler.database;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+import com.kwfisher.vacationscheduler.dao.ExcursionDAO;
+import com.kwfisher.vacationscheduler.dao.VacationDAO;
 import com.kwfisher.vacationscheduler.entities.Excursion;
 import com.kwfisher.vacationscheduler.entities.Vacation;
 
 @Database(entities = {Vacation.class, Excursion.class}, version = 1, exportSchema = false)
 public abstract class VacationDatabaseBuilder extends RoomDatabase {
+
+    public abstract VacationDAO vacationDAO();
+    public abstract ExcursionDAO excursionDAO();
+    private static volatile VacationDatabaseBuilder INSTANCE;
 }
