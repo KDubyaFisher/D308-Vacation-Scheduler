33643bd548887c0dc33c3e45fafc490035a84127
kwfisher <fisher.kolton@gmail.com>
2025-09-19 17:36:02 -0500

    feat(database): add CRUD methods to Repository class

--- app/src/main/java/com/kwfisher/vacationscheduler/database/Repository.java
+++ app/src/main/java/com/kwfisher/vacationscheduler/database/Repository.java
@@ -28,4 +28,117 @@ public class Repository {
         mVacationDAO = db.vacationDAO();
         mExcursionDAO = db.excursionDAO();
     }
+
+    public List<Vacation> getAllVacations(){
+        databaseExecutor.execute(() -> {
+            mAllVacations = mVacationDAO.getAllVacations();
+        });
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return mAllVacations;
+    }
+
+    public void insert(Vacation vacation){
+        databaseExecutor.execute(() -> {
+            mVacationDAO.insert(vacation);
+        });
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void update(Vacation vacation){
+        databaseExecutor.execute(() -> {
+            mVacationDAO.update(vacation);
+        });
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void delete(Vacation vacation){
+        databaseExecutor.execute(() -> {
+            mVacationDAO.delete(vacation);
+        });
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Excursion> getAllExcursions(){
+        databaseExecutor.execute(()->{
+            mAllExcursions = mExcursionDAO.getAllExcursions();
+        });
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+        return mAllExcursions;
+    }
+
+    public List<Excursion> getAssociatedExcursions(int vacationID){
+        databaseExecutor.execute(()->{
+            mAllExcursions = mExcursionDAO.getExcursionsForVacation(vacationID);
+        });
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+        return mAllExcursions;
+    }
+
+    public void insert(Excursion excursion){
+        databaseExecutor.execute(() -> {
+            mExcursionDAO.insert(excursion);
+        });
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void update(Excursion excursion){
+        databaseExecutor.execute(() -> {
+            mExcursionDAO.update(excursion);
+        });
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void delete(Excursion excursion){
+        databaseExecutor.execute(() -> {
+            mExcursionDAO.delete(excursion);
+        });
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+
+
 }
