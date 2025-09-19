a90d4ea67a92925ba46f7d0a60851541f16cf216
kwfisher <fisher.kolton@gmail.com>
2025-09-19 16:17:55 -0500

    feat(entities): add fields to Excursion.java

--- app/src/main/java/com/kwfisher/vacationscheduler/entities/Excursion.java
+++ app/src/main/java/com/kwfisher/vacationscheduler/entities/Excursion.java
@@ -1,7 +1,14 @@
 package com.kwfisher.vacationscheduler.entities;
 
 import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 @Entity(tableName = "excursions")
 public class Excursion {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String excursionTitle;
+    private String excursionDate;
+    private int vacationID;
 }
