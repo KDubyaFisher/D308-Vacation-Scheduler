a265834e1e9dbfbb59e30db2b972fdd9b89b44f4
kwfisher <fisher.kolton@gmail.com>
2025-09-19 16:19:10 -0500

    feat(entities): add constructor to Excursion.java

--- app/src/main/java/com/kwfisher/vacationscheduler/entities/Excursion.java
+++ app/src/main/java/com/kwfisher/vacationscheduler/entities/Excursion.java
@@ -11,4 +11,12 @@ public class Excursion {
     private String excursionTitle;
     private String excursionDate;
     private int vacationID;
+
+
+    public Excursion(int id, String excursionTitle, String excursionDate, int vacationID) {
+        this.id = id;
+        this.excursionTitle = excursionTitle;
+        this.excursionDate = excursionDate;
+        this.vacationID = vacationID;
+    }
 }
