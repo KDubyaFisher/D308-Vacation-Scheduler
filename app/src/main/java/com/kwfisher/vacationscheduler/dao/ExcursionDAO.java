9effc25df21013224a0cf3ed2e5cc8ca64ef4463
kwfisher <fisher.kolton@gmail.com>
2025-09-19 16:51:58 -0500

    feat(dao): add CRUD operations to ExcursionDAO

--- app/src/main/java/com/kwfisher/vacationscheduler/dao/ExcursionDAO.java
+++ app/src/main/java/com/kwfisher/vacationscheduler/dao/ExcursionDAO.java
@@ -1,7 +1,31 @@
 package com.kwfisher.vacationscheduler.dao;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.kwfisher.vacationscheduler.entities.Excursion;
+
+import java.util.List;
 
 @Dao
 public interface ExcursionDAO {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Excursion excursion);
+
+    @Update
+    void update(Excursion excursion);
+
+    @Delete
+    void delete(Excursion excursion);
+
+    @Query("SELECT * FROM excursions ORDER BY id ASC")
+    List<Excursion> getAllExcursions();
+
+    @Query("SELECT * FROM excursions WHERE vacationID = :vacationID ORDER BY id ASC")
+    List<Excursion> getExcursionsForVacation(int vacationID);
 }
