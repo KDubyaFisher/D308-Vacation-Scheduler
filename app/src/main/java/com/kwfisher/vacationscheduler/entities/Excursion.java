dcd9a469c9ac6e99dd34c673e1659b919642ddb5
kwfisher <fisher.kolton@gmail.com>
2025-09-19 16:20:05 -0500

    feat(entities): add getters and setters to Excursion.java

--- app/src/main/java/com/kwfisher/vacationscheduler/entities/Excursion.java
+++ app/src/main/java/com/kwfisher/vacationscheduler/entities/Excursion.java
@@ -19,4 +19,36 @@ public class Excursion {
         this.excursionDate = excursionDate;
         this.vacationID = vacationID;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getExcursionTitle() {
+        return excursionTitle;
+    }
+
+    public void setExcursionTitle(String excursionTitle) {
+        this.excursionTitle = excursionTitle;
+    }
+
+    public String getExcursionDate() {
+        return excursionDate;
+    }
+
+    public void setExcursionDate(String excursionDate) {
+        this.excursionDate = excursionDate;
+    }
+
+    public int getVacationID() {
+        return vacationID;
+    }
+
+    public void setVacationID(int vacationID) {
+        this.vacationID = vacationID;
+    }
 }
