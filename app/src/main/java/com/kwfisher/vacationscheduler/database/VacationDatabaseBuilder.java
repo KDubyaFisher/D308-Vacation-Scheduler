ba08a23feb3c0e066b8aeca9ea81bd13afdc9a32
kwfisher <fisher.kolton@gmail.com>
2025-09-19 16:58:30 -0500

    refactor(database): extend RoomDatabase in VacationDatabaseBuilder

--- app/src/main/java/com/kwfisher/vacationscheduler/database/VacationDatabaseBuilder.java
+++ app/src/main/java/com/kwfisher/vacationscheduler/database/VacationDatabaseBuilder.java
@@ -1,10 +1,11 @@
 package com.kwfisher.vacationscheduler.database;
 
 import androidx.room.Database;
+import androidx.room.RoomDatabase;
 
 import com.kwfisher.vacationscheduler.entities.Excursion;
 import com.kwfisher.vacationscheduler.entities.Vacation;
 
 @Database(entities = {Vacation.class, Excursion.class}, version = 1, exportSchema = false)
-public class VacationDatabaseBuilder {
+public abstract class VacationDatabaseBuilder extends RoomDatabase {
 }
