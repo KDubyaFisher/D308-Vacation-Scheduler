575da5a68ac4e82d254097f571c48ceb83a598a4
kwfisher <fisher.kolton@gmail.com>
2025-09-19 13:48:22 -0500

    feat(ui): add FAB to VacationDetails to navigate to ExcursionDetails

--- app/src/main/java/com/kwfisher/vacationscheduler/UI/VacationDetails.java
+++ app/src/main/java/com/kwfisher/vacationscheduler/UI/VacationDetails.java
@@ -1,6 +1,8 @@
 package com.kwfisher.vacationscheduler.UI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +10,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.kwfisher.vacationscheduler.R;
 
 public class VacationDetails extends AppCompatActivity {
@@ -22,5 +25,16 @@ public class VacationDetails extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton2);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(VacationDetails.this, ExcursionDetails.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
